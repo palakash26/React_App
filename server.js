2f2b82f700e5703ac434056702391143bfa3a424
palakash26 <pala68771@gmail.com>
2025-06-10 13:59:49 +0530

    job api

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ import dotenv from "dotenv";
 import { initDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import transtionRoute  from "./routes/transtionRouter.js";
+import job from "./config/cron.js";
 dotenv.config();
 
 const app = express();
+
+if (process.env.NODE_ENV === "production") job.start(); // Start the cron job if in production
+// Set the port from environment variable or default to 5002
 const Port = process.env.PORT || 5002;
 
 // Miidleware
@@ -13,7 +17,12 @@ app.use(rateLimiter)
 app.use(express.json());
 
 
-
+app.get("/api/health", (req, res) => {
+  res.status(200).json({
+    message: "Welcome to the Expense Tracker API",
+    status: "success",
+  });
+});
 
 app.use("/api/transactions",transtionRoute);
 
